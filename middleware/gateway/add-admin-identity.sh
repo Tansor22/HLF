7d7611cb3ecc8e8ff07041f1b4550acd4ea98c59
Sergei <lyutyy.mail@gmail.com>
2021-03-11 10:50:37 +0700

    Scripts edited.

--- middleware/gateway/add-admin-identity.sh
+++ middleware/gateway/add-admin-identity.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env node
+#!/bin/bash
 # shellcheck disable=SC2164
-cd middleware/gateway
+cd middleware
+# sh ./clean.sh
+rm -rf node_modules     &> /dev/null
+rm package-lock.json    &> /dev/null
+npm install
+cd gateway
+
 # пока что делаем все от админа
 node wallet.js add acme Admin && exit
