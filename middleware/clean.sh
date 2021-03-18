27b249c85a0233e5cc9ec8f733648f368c5c2888
Sergei <lyutyy.mail@gmail.com>
2021-03-18 14:30:59 +0700

    clean.sh edited

--- middleware/clean.sh
+++ middleware/clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Script for cleaning the sample code folder - to be executed in VM
 # Updated : April 2020
-
+# shellcheck disable=SC2164
+cd middleware
 rm -rf node_modules     &> /dev/null
 rm package-lock.json    &> /dev/null
 
