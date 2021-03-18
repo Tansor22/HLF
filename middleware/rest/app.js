db154d3ac83a4456581bf266ed5fd9988a0a9090
Sergei <lyutyy.mail@gmail.com>
2021-03-19 00:23:18 +0700

    middleware endpoints added

--- middleware/rest/app.js
+++ middleware/rest/app.js
@@ -25,26 +25,55 @@ async function main() {
     await setupGateway()
     var jsonParser = bodyParser.json()
     app.post('/newDoc', jsonParser, async function (req, res) {
+       // todo add user selection in each func
         let body = req.body
         console.log("Request = " + body)
         let network = await gateway.getNetwork(NETWORK_NAME)
         const contract = await network.getContract(CONTRACT_ID);
-        try {
-            // Submit the transaction
-            let response = await contract.submitTransaction('new-doc', body.org, body.content, JSON.stringify(body.signsRequired))
-            console.log("Submit Response=", response.toString())
-            res.setHeader('Content-Type', 'application/json');
-            res.end(response)
-        } catch (e) {
-            console.log(e)
-            res.end("FAILED, see logs of middleware")
-        }
+        await invoke(req, res, contract, 'new-doc', body.org, body.content, JSON.stringify(body.signsRequired))
+    })
+    app.post('/getDocs', jsonParser, async function (req, res) {
+        let body = req.body
+        console.log("Request = " + body)
+        let network = await gateway.getNetwork(NETWORK_NAME)
+        const contract = await network.getContract(CONTRACT_ID);
+        await query(req, res, contract, 'get-docs', body.orgName)
+    })
+    app.post('/signDoc', jsonParser, async function (req, res) {
+        let body = req.body
+        console.log("Request = " + body)
+        let network = await gateway.getNetwork(NETWORK_NAME)
+        const contract = await network.getContract(CONTRACT_ID);
+        await invoke(req, res, contract, 'sign-doc', body.documentId, JSON.stringify(body.signs))
     })
     var server = app.listen(8081, function () {
         var host = server.address().address
         var port = server.address().port
         console.log("REST server listening at http://%s:%s", host, port)
     })
+}
+async function query(req, res, contract, func, ... args){
+    try{
+        let response = await contract.evaluateTransaction(func, ... args)
+        console.log(`Query Response=${response.toString()}`)
+        res.setHeader('Content-Type', 'application/json');
+        res.end(response)
+    } catch(e){
+        console.log(e)
+        res.end("FAILED, see logs of middleware")
+    }
+}
+
+async function invoke(req, res, contract, func, ... args){
+    try{
+        let response = await contract.submitTransaction(func, ... args)
+        console.log(`Query Response=${response.toString()}`)
+        res.setHeader('Content-Type', 'application/json');
+        res.end(response)
+    } catch(e){
+        console.log(e)
+        res.end("FAILED, see logs of middleware")
+    }
 }
 
 async function setupGateway() {
